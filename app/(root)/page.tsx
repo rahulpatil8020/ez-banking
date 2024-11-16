9c219d3fef4aaf05bbbaacd06531172ebf4ff5ab
Rahul Patil <rahuldpatil20@gmail.com>
2024-11-15 20:56:38 -0800

    Created home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,30 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = {
+    firstName: "Rahul",
+  };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your accounts"
+          />
+        </header>
+        <TotalBalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={100.45}
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Home;

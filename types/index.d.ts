5b1f4123d066805808c571b301150c38b7ad473d
Rahul Patil <rahuldpatil20@gmail.com>
2024-11-15 20:57:32 -0800

    created types

--- types/index.d.ts
+++ types/index.d.ts
@@ -202,7 +202,7 @@ declare interface BankTabItemProps {
   appwriteItemId?: string;
 }
 
-declare interface TotlaBalanceBoxProps {
+declare interface TotleBalanceBoxProps {
   accounts: Account[];
   totalBanks: number;
   totalCurrentBalance: number;

10dd0ba64a29bd74e79713a6cfcea2c400e66bf5
Rahul Patil <rahuldpatil20@gmail.com>
2024-11-15 20:56:30 -0800

    Created root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,33 @@
+import MobileNavbar from "@/components/MobileNavbar";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = {
+    firstName: "Rahul",
+    lastName: "Patil",
+  };
   return (
-    <main>
-      SIDEBAR
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image
+            src="/icons/logo.svg"
+            alt="EZ Bank logo"
+            width={30}
+            height={30}
+          />
+          <div>
+            <MobileNavbar user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
